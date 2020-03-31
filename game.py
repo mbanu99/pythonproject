d9815882eab9fae056e1fe1500aaaa7b41008a20
root <root@ansible.ziyotek.local>
2020-03-30 21:39:21 -0400

    added new changes

--- game.py
+++ game.py
@@ -1,3 +1,15 @@
 # Author: Madina Banu
 # Contact: bellabanu888@gmail.com
 # A fun little python game
+
+name = input("What is your name")
+age = int(input("What is your age"))
+your_age_in_hundred_years = age+100
+like_pizza = True
+
+print("Hi," + name + "!")
+print("Your age is " + str(age) + "but in 100 years, you are going to be" + str(your_age_in_hundred_years))
+
+if(like_pizza):
+   print("Hello")
+   
